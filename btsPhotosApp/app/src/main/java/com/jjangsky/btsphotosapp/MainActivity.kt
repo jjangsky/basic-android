b8f1ac28145ec297a297aa6485ac6a5da23b58d9
jjangsky <dbcks4646@gmail.com>
2025-02-16 23:45:24 +0900

    feat: 첫번째 사진 클릭 시, 화면 전환 이벤트 처리

--- btsPhotosApp/app/src/main/java/com/jjangsky/btsphotosapp/MainActivity.kt
+++ btsPhotosApp/app/src/main/java/com/jjangsky/btsphotosapp/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.jjangsky.btsphotosapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 1. 화면이 클릭되었다는 것을 알아야 함
+        // -> xml에서 id 속성값을 가져온 것을 코틀린에서 변수에 바인딩 처리
+        val image1 = findViewById<ImageView>(R.id.bts_image_1)
+        image1.setOnClickListener{
+
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게 보여줌!
+            val intent = Intent(this, BtsDetailActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
